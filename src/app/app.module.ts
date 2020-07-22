d2cfe3e528f5dab73918660d38313ef3d5a8d7d6
zakhariak <azakhariak@gmail.com>
2020-07-22 12:17:41 +0300

    corrected code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,34 +4,12 @@ import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-//HOMEWORK1 <<<
-// import { HeaderComponent } from './homework1/header/header.component';
-// import { MainComponent } from './homework1/main/main.component';
-//>>>
-
-//HOMEWORK2 <<<
-// import { Homework2Component } from './homework2/homework2.component';
-//>>>
-
-//HOMEWORK3 <<<
 import { Homework3Component } from './homework3/homework3.component';
-//>>>
 
 @NgModule({
   declarations: [
     AppComponent,
-    //HOMEWORK1 <<<
-    // HeaderComponent,
-    // MainComponent,
-    //>>>
-
-    //HOMEWORK2 <<<
-    // Homework2Component,
-    //>>>
-
-    //HOMEWORK3 <<<
-    Homework3Component,
-    //>>>
+    Homework3Component
   ],
   imports: [
     BrowserModule,
